37107a44be3ddcc138c1a8b06aaa2d991ba8c50a
dhzdhd <dhzdhd@proton.me>
2024-02-28 22:24:34 +0530

    Add adopt option

--- init.sh
+++ init.sh
@@ -3,9 +3,17 @@
 echo "Setup for desktop(1), WM only desktop(2), server(3)"
 read -r home
 
+echo "Adopt pre-existing config? (y/n)"
+read -r adopt
+
 case "$home" in
 1)
-    exec stow --target="$HOME" --dir=./configs/linux/ common
+    if [[ "${adopt}" == "y" ]]; then
+        exec stow --adopt --target="$HOME" --dir=./configs/linux/ common
+    else
+        exec stow --target="$HOME" --dir=./configs/linux/ common
+    fi
+
     ;;
 2)
     echo ""
